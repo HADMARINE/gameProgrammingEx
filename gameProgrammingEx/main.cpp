b88135c50a5cbab1c60e83b05d868f8360d3a0a9
Lee Hojoon <tsx@kakao.com>
2020-04-27 10:22:23 +0900

    Initial Commit

---
+++ gameProgrammingEx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gameProgrammingEx
+//
+//  Created by 이호준 on 2020/04/27.
+//  Copyright © 2020 HADMARINE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
